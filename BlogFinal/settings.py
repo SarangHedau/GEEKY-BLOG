838a3d465818476cdf4492cd29a18ef510e145dd
Your Name <steve22@gmail.com>
2023-06-26 19:51:56 +0530

    first commit

--- BlogFinal/settings.py
+++ BlogFinal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e86=07i62!(b6n(8*6m)exqky!93*&#t3b##_w37mzfm8-^gtr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
